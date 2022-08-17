e8f3a0daf40c9c9bd0d8747f06bf2df793be8850
Matt Decuir <code@experimatt.com>
2022-08-17 10:42:06 -0500

    Add player class & tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require './lib/player'
+
+describe Player do
+  let(:c1) { Card.new(:diamond, 'Queen', 12) }
+  let(:c2) { Card.new(:spade, '3', 3) }
+  let(:c3) { Card.new(:heart, 'Ace', 14)   }
+  let(:cards) { [c1, c2, c3] }
+  let(:deck) { Deck.new(cards) }
+
+  it 'exists' do
+    player = Player.new('boop', Deck.new)
+    expect(player).to be_an_instance_of(Player)
+  end
+
+  it 'has readable attributes' do
+    player = Player.new('boop', deck)
+
+    expect(player.name).to eq('boop')
+    expect(player.deck).to eq(deck)
+  end
+
+  describe '#has_lost?' do
+    it 'returns false when deck has cards' do
+      player = Player.new('boop', deck)
+
+      expect(player.has_lost?).to eq(false)
+    end
+
+    it 'returns true when deck has no cards' do
+      player = Player.new('boop', Deck.new)
+
+      expect(player.has_lost?).to eq(true)
+    end
+  end
+
+end
